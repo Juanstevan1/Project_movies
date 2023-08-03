7eabe331a301da2a31d871750adf18d83b045371
JuanGarcia6 <jegarciag1@eafit.edu.co>
2023-08-03 17:30:55 -0500

    Parte 4 del proyecto

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Juan Esteban'})
+
+def about(request):
+    return render(request, 'about.html', {'name': 'Juan Esteban', 'age': '19'})
