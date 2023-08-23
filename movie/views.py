a5fccd26bbe978acd918890c655284f7a46fae9f
JuanGarcia6 <jegarciag1@eafit.edu.co>
2023-08-23 17:19:22 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from .models import Movie
 # Create your views here.
-
 def home(request):
     searchTerm = request.GET.get('SearchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm1':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm1':searchTerm})
 
 def about(request):
     return render(request, 'about.html', {'name': 'Juan Esteban', 'age': '19'})
